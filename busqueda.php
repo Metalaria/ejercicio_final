2c933a8e0867ea040629325d3f3da1c0b32518dc
metalaria <gonzalo.mejias.moreno@gmail.com>
2013-11-10 18:17:27 +0100

    create file to see which exercises are booked

---
+++ busqueda.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+if (!isset($_SESSION['id_user'])) {
+            echo "No ha iniciado sesi&oacute;n";
+            header("refresh: 3; url= index.php");   
+        } else{
+
+$con = new mysqli('localhost', 'root', '', 'prueba');
+
+$busca = mysqli_query($con, " SELECT ejercicio FROM ejercicios WHERE reservado= 's'"); 
+    while ($f =  mysqli_fetch_array($busca)){
+        echo $f ['ejercicio']. '&nbsp;'. '&nbsp;'."<br/>" ; 
+    }
+    echo " <p><a href='exito.php'>volver atr&aacute;s</a></p>";
+        
+        }
+?>
