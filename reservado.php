c4817f81b587a0007aa6b222666ebc358d74b0e5
metalaria <gonzalo.mejias.moreno@gmail.com>
2013-11-10 15:23:32 +0100

    add a comment

--- reservado.php
+++ reservado.php
@@ -11,7 +11,7 @@ $con = new mysqli('localhost', 'root', '', 'prueba');
 if(empty($_REQUEST["reservar_ejercicio"])){
     echo "No ha introducido ningún ejercicio";
 }
-
+// el implode sirve para reservar 1 o más ejercicios al mismo tiempo
 if(isset($_REQUEST["reservar_ejercicio"])) {
     $q_res = "update ejercicios set reservado = 's' where ejercicio in ('" . implode("','", $_POST['reservar_ejercicio']) . "')";
     mysqli_query($con, $q_res);
