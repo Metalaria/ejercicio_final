d04ccaec50a577da8dcb374ebcd1bc64226fa196
metalaria <gonzalo.mejias.moreno@gmail.com>
2013-11-12 16:11:19 +0100

    add option to switch between different tipes of users

--- validacion2.php
+++ validacion2.php
@@ -3,17 +3,28 @@
 $con = new mysqli('localhost', 'root', '', 'prueba');
 //recibe los datos y hace la consulta
 $user = $_POST['user1'];
+
 $pass = $_POST['pass1'];
-//realiza la consulta para realizar la conexion
-$queUser = mysqli_query( $con,"select usuario from usuarios where password = '$pass'");
+
+$queUser = mysqli_query( $con,"select id_user, tipo from usuarios where password = '$pass'");
 
 $fila = mysqli_fetch_assoc($queUser);
 
-$contra = $fila['usuario'];
-// si los datos son correctos redirige a exito.php
-if ($user == $contra) {
-    header('location:exito.php');
-} else {
+$contra = $fila['id_user'];
+echo $contra."<br/>";
+$tipo_user = $fila['tipo'];
+echo $tipo_user;
+
+if ($user === $contra and $tipo_user == 1 )  {
+        header('location: exito.php');
+    }else{
+    echo "<br/>"."usuario  o contrase&ntilde;a incorrecta  <br/>";
+    echo "Redirigiendo a la p&aacute;gina principal ...<br/>";
+    header('refresh: 3; url= index.php');
+    }
+   if ($user === $contra and $tipo_user == 0){
+       header ('location: alumno.php');
+    } else {
     echo "<br/>"."usuario  o contrase&ntilde;a incorrecta  <br/>";
     echo "Redirigiendo a la p&aacute;gina principal ...<br/>";
     header('refresh: 3; url= index.php');
