bacee8a20bddce4947538ecaf07a0f7fc9baea6c
metalaria <gonzalo.mejias.moreno@gmail.com>
2013-11-12 16:12:38 +0100

    Change file. This is what the proffessor will see

--- exito.php
+++ exito.php
@@ -13,11 +13,11 @@
             echo "No ha iniciado sesi&oacute;n";
             header("refresh: 3; url= index.php");   
         } else{
-            echo " <p><a href='reservar.php'>Reservar ejercicio</a></p>";
-            echo " <p><a href='busqueda.php'>Ver ejercicios que ya est&aacute;n reservados</a></p>";
-            
-            // este formulario sirve para destruir la sesión y desconectarse
-              echo"  <form name= 'form1' action='desconexion.php' method='POST' >
+            "<p><a href='registrar_ropa.php'>a&ntilde;adir una prenda</a></p>
+                <p><a href='modificador_ropa.php'>Modificar una prenda</a></p>
+                <p><a href='buscador_ropa.php'>Buscar una prenda</a></p>
+                <p><a href='eliminar_ropa.php'>Eliminar una prenda</a></p>
+                <form name= 'form1' action='desconexion.php' method='POST' >
                     <input type='submit' name='salir' value='salir' />
                 </form>";
         }
